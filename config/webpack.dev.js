d5b572beac80bab83b1df53a81b81e325616e8e8
v_liukai01 <v_liukai01@baidu.com>
2019-03-22 09:55:09 +0800

    feat:添加插件

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,19 +1,48 @@
-const path = require ("path")
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+
 module.exports = {
-    mode:'development',
-    entry:{
-        main:'./src/main.js'
+    mode: 'development',
+    entry: [
+        'react-hot-loader/patch',   //热更新
+        './src/main.js'
+    ],
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        filename: '[name].[hash:8].js'
     },
-    output:{
-        path:path.resolve(__dirname,'../dist'),
-        filename:'[name].bundle.js'
+    module: {
+        rules: [
+            // js文件import css
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
     },
-    module:{},
-    plugins:[],
-    devServer:{
-        contentBase:path.resolve(__dirname,'../dist'),
-        host:'localhost',
-        compress:true,
-        port:8888
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            minify: {
+                collapseWhitespace: true     //把生成的 index.html 文件的内容的没用空格去掉，减少空间
+            },
+            title: "webpack-study",
+            hash: true,              //清缓存用
+            hot: true,       //热更新
+        }),
+        new CleanWebpackPlugin(),         //实例化，不加参数，清除多余参数
+        new webpack.HotModuleReplacementPlugin()      //热更新
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, '../dist'),   //指定打开的页面
+        host: 'localhost',
+        compress: true,
+        port: 8888
     }
 }
---
+++ src/main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "./index.css";
+
+// ReactDOM.render(
+//     <App />,
+//     document.getElementById('root')
+// )
+//
+//
+// // Hot Module Replacement API
+// if (module.hot) {
+//     module.hot.accept()
+// }
+
+
+
+
